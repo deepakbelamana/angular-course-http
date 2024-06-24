e18ae7884fa512d5d33a266ad1b6a32438c9892c
deepak belamana <saideepak627@gmail.com>
2024-06-24 08:00:22 +0530

    using types in http requests

---
+++ src/app/app.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Posts } from './posts.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  loadedPosts = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchPosts()
+  }
+
+  onCreatePost(postData: Posts) {
+    // Send Http request
+    this.http.post<{name:string}>('https://angular-course-db-b6f6a-default-rtdb.firebaseio.com/posts.json'
+      ,postData
+    ).subscribe(
+      responseData => console.log(responseData)
+    )
+  }
+
+  onFetchPosts() {
+    // Send Http request
+    this.fetchPosts()
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
+
+  fetchPosts()
+  {
+    this.http.get<{[key:string]:Posts}>(
+      'https://angular-course-db-b6f6a-default-rtdb.firebaseio.com/posts.json'
+    ).pipe(map(
+      responseData =>{
+       const responseArray=[]
+       for(const key in responseData){
+        if(responseData.hasOwnProperty(key)) {
+          responseArray.push({...responseData[key],id:key})
+        }
+       }
+       return responseArray
+      }
+    ))
+    .subscribe(
+      responseData => console.log(responseData)
+    )
+  }
+}
